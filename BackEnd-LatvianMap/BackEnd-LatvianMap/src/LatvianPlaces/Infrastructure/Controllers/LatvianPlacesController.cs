658ad3a93fca449fd18afc142c39bfae0dfc1e5c
Casher1no <79166585+Casher1no@users.noreply.github.com>
2023-01-26 23:55:17 +0200

    CSV reader

--- BackEnd-LatvianMap/BackEnd-LatvianMap/src/LatvianPlaces/Infrastructure/Controllers/LatvianPlacesController.cs
+++ BackEnd-LatvianMap/BackEnd-LatvianMap/src/LatvianPlaces/Infrastructure/Controllers/LatvianPlacesController.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using BackEnd_LatvianMap.LatvianPlaces.Model;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,21 +19,47 @@ namespace BackEnd_LatvianMap.LatvianPlaces.Infrastructure.Controllers
     public class LatvianPlacesController : ControllerBase
     {
         [HttpGet(Name = "latvia/places")]
-        public void GetPlaces()
+        public string GetPlaces()
         {
-            string uri =
-                "https://data.gov.lv/dati/dataset/0c5e1a3b-0097-45a9-afa9-7f7262f3f623/resource/1d3cbdf2-ee7d-4743-90c7-97d38824d0bf/download/aw_csv.zip";
-            
-            WebClient webClient = new WebClient();
-            webClient.Headers.Add("Accept: text/html, application/xhtml+xml, */*");
-            webClient.Headers.Add("User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)");
-            
-            webClient.DownloadFileAsync(new Uri(uri),"Downloads/resource.zip");
+            string uri = "https://data.gov.lv/dati/dataset/0c5e1a3b-0097-45a9-afa9-7f7262f3f623/resource/1d3cbdf2-ee7d-4743-90c7-97d38824d0bf/download/aw_csv.zip";
 
-            using (ZipArchive)
+            string zipPath = "Downloads/Zip/resource.zip";
+
+            string csvDataFile = "AW_VIETU_CENTROIDI.CSV";
+
+            using (WebClient client = new WebClient())
             {
-                
+                client.DownloadFile(uri,zipPath);
             }
+            
+            using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.FullName == csvDataFile)
+                    {
+                        var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+                        {
+                            Delimiter = "#",
+                        };
+                        
+                        using (var reader = new StreamReader(entry.Open()))
+                        using (var csv = new CsvReader(reader, csvConfig))
+                        {
+                            var records = csv.GetRecords<Centroids>().ToList();
+                            foreach (var record in records)
+                            {
+                                return record.STD;
+                            }
+                            
+                        }
+                            
+                        
+                    }
+                }
+            }
+
+            return "none";
 
         }
     }
---
+++ BackEnd-LatvianMap/BackEnd-LatvianMap/src/LatvianPlaces/Model/Centroids.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace BackEnd_LatvianMap.LatvianPlaces.Model;
+
+public class Centroids
+{
+    public int KODS { get; set; }
+    public int TIPS_CD { get; set; }
+    public string NOSAUKUMS { get; set; }
+    public int VKUR_CD { get; set; }
+    public int VKUR_TIPS { get; set; }
+    public string STD { get; set; }
+    public float KOORD_X { get; set; }
+    public float KOORD_Y { get; set; }
+    public float DD_N { get; set; }
+    public float DD_E { get; set; }
+}
