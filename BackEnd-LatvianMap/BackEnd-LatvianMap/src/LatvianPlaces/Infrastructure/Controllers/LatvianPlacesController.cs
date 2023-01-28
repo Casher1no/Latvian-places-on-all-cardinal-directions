1238b4acc787db740cf8822c2c7478b625bee461
Casher1no <79166585+Casher1no@users.noreply.github.com>
2023-01-28 14:39:24 +0200

    Furthest by palces API

--- BackEnd-LatvianMap/BackEnd-LatvianMap/src/LatvianPlaces/Infrastructure/Controllers/LatvianPlacesController.cs
+++ BackEnd-LatvianMap/BackEnd-LatvianMap/src/LatvianPlaces/Infrastructure/Controllers/LatvianPlacesController.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using BackEnd_LatvianMap.LatvianPlaces.Model;
 using CsvHelper;
@@ -14,53 +16,66 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BackEnd_LatvianMap.LatvianPlaces.Infrastructure.Controllers
 {
-    [Route("api/latvia/places")]
+    [Route("api/latvia/further-places")]
     [ApiController]
     public class LatvianPlacesController : ControllerBase
     {
-        [HttpGet(Name = "latvia/places")]
-        public string GetPlaces()
+        [HttpGet]
+        public Dictionary<string,Centroids> GetFurtherPlaces()
         {
-            string uri = "https://data.gov.lv/dati/dataset/0c5e1a3b-0097-45a9-afa9-7f7262f3f623/resource/1d3cbdf2-ee7d-4743-90c7-97d38824d0bf/download/aw_csv.zip";
+            string uri =
+                "https://data.gov.lv/dati/dataset/0c5e1a3b-0097-45a9-afa9-7f7262f3f623/resource/1d3cbdf2-ee7d-4743-90c7-97d38824d0bf/download/aw_csv.zip";
 
             string zipPath = "Downloads/Zip/resource.zip";
 
-            string csvDataFile = "AW_VIETU_CENTROIDI.CSV";
+            string csvDataFile = "/AW_VIETU_CENTROIDI.CSV";
+
+            string extracted = "Downloads/extracted";
+
+            string tempFile = "Downloads/temp";
+
+            Centroids place = new Centroids();
 
             using (WebClient client = new WebClient())
             {
-                client.DownloadFile(uri,zipPath);
+                client.DownloadFile(uri, zipPath);
             }
-            
+
             using (ZipArchive archive = ZipFile.OpenRead(zipPath))
             {
-                foreach (ZipArchiveEntry entry in archive.Entries)
+                var entry = archive.GetEntry(csvDataFile);
+
+                if (!Directory.Exists(extracted))
                 {
-                    if (entry.FullName == csvDataFile)
-                    {
-                        var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
-                        {
-                            Delimiter = "#",
-                        };
-                        
-                        using (var reader = new StreamReader(entry.Open()))
-                        using (var csv = new CsvReader(reader, csvConfig))
-                        {
-                            var records = csv.GetRecords<Centroids>().ToList();
-                            foreach (var record in records)
-                            {
-                                return record.STD;
-                            }
-                            
-                        }
-                            
-                        
-                    }
+                    archive.ExtractToDirectory(extracted);
+                }
+
+                string csvFileContent = System.IO.File.ReadAllText(extracted + csvDataFile);
+                string cleanedFileContent = Regex.Replace(csvFileContent, "#", "");
+                System.IO.File.WriteAllText(tempFile, cleanedFileContent);
+
+                using (var streamReader = new StreamReader(tempFile))
+                using (var csvReader = new CsvReader(streamReader, new CsvConfiguration(CultureInfo.InvariantCulture)
+                       {
+                           Delimiter = ";",
+                           BadDataFound = null
+                       }))
+                {
+                    csvReader.Read();
+                    csvReader.ReadHeader();
+
+                    var centroids = csvReader.GetRecords<Centroids>().ToList();
+
+                    Dictionary<string,Centroids> centroidsList = new Dictionary<string, Centroids>();
+                    
+                    centroidsList.Add("furthestNorth",centroids.MaxBy(c => c.DD_N));
+                    centroidsList.Add("furthestSouth",centroids.MinBy(c => c.DD_N));
+                    centroidsList.Add("furthestEast",centroids.MaxBy(c => c.DD_E));
+                    centroidsList.Add("furthestWest",centroids.MinBy(c => c.DD_E));
+                    
+                    return centroidsList;
                 }
             }
-
-            return "none";
-
         }
     }
 }
